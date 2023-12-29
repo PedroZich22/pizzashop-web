7b8e5af22348a2daf5cde82e63c904aba20515ad
Pedro <zichpedro22@gmail.com>
2023-12-28 22:25:08 -0300

    chore: helmet install and configuration

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export function SignIn() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1>SignIn</h1>
-    </div>
+    <>
+      <Helmet title="Login" />
+      <h1>Login</h1>
+    </>
   )
 }
